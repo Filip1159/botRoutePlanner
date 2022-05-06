12e1e6400e856a40e38caad4e21894235330c855
Filip1159 <fwfw541111@gmail.com>
2022-05-06 20:18:04 +0200

    Prepare for user console input

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,14 +4,21 @@ import botrouteplanner.model.Path;
 import java.io.IOException;
 
 public class Main {
-    static final String GRID_PATH = "C:/Users/PAVILION/IdeaProjects/BotRoutePlanner/src/main/resources/grid-2.txt";
-    static final String JOB_PATH = "C:/Users/PAVILION/IdeaProjects/BotRoutePlanner/src/main/resources/job-2.txt";
-
-    public static void main(String[] args) throws IOException {
-        Planner planner = Planner.load(GRID_PATH, JOB_PATH);
-        Path path = planner.createRoute();
-        System.out.println(path.getTransitionsCount());
-        System.out.println(path.getTravelTime());
-        path.printFormatted();
+    public static void main(String[] args) {
+        if (args.length != 2)
+            System.out.println("Error: app requires exactly 2 arguments");
+        else {
+            String gridFilePath = args[0];
+            String jobFilePath = args[1];
+            try {
+                Planner planner = Planner.load(gridFilePath, jobFilePath);
+                Path path = planner.createRoute();
+                System.out.println(path.getTransitionsCount());
+                System.out.println(path.getTravelTime());
+                path.printFormatted();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
